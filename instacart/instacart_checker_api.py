f7d4a967ab0c2fb35048ab4852e63401540c5066
chrismonsen <chris.monsen@gmail.com>
2020-04-15 22:57:15 -0400

    initial commit

--- instacart/instacart_checker_api.py
+++ instacart/instacart_checker_api.py
@@ -7,6 +7,5 @@ def check_and_notify(store, phone_number, cookie):
     url = instacart_checker_api_url + "/checkAndNotify"
     payload = "{\"phoneNumber\":\"" + phone_number + "\", \"store\": \"" + store + "\"}"
     headers = {"Content-Type": "application/json", "X-Cookie": cookie}
-    print(headers)
     r = requests.post(url, headers=headers, data=payload)
     return r
