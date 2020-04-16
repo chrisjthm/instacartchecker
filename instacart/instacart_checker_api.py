227c2627101c735cdf25ef8899e9915ae6046b21
chrismonsen <chris.monsen@gmail.com>
2020-04-15 22:56:54 -0400

    initial commit

--- instacart/instacart_checker_api.py
+++ instacart/instacart_checker_api.py
@@ -6,5 +6,7 @@ instacart_checker_api_url = "https://instacart-checker-api.herokuapp.com"
 def check_and_notify(store, phone_number, cookie):
     url = instacart_checker_api_url + "/checkAndNotify"
     payload = "{\"phoneNumber\":\"" + phone_number + "\", \"store\": \"" + store + "\"}"
-    r = requests.post(url, headers={"Content-Type": "application/json", "Cookie": cookie}, data=payload)
+    headers = {"Content-Type": "application/json", "X-Cookie": cookie}
+    print(headers)
+    r = requests.post(url, headers=headers, data=payload)
     return r
